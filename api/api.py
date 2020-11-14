50f5c577da95667ab865aa2cf98d715870caef96
fabiodemo <fabiodemo@redes.ufsm.br>
2020-11-14 15:38:06 -0300

    Adicionado o "app.py" para redirecionamento da endpoint, além de meios para a do agendamento de evento no banco de dados, especificação da resposta Json.

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api, reqparse
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+api = Api(app)
 
 mysql = MySQL()
 
@@ -27,12 +29,26 @@ class CreateEvent(Resource):
             parser.add_argument('sms', type=str, help='title to create event')
             parser.add_argument('push', type=str, help='title to create event')
             parser.add_argument('whatsapp', type=str, help='title to create event')
+            args = parser.parse_args()
 
+            _eventTitle = args['title']
+            _eventDesc = args['description']
+            _eventDate = args['date']
+            _eventTime = args['time']
+            _eventMeans = args['email']
+
+            cursor.execute("insert into tblCommunication (Title, Description) values ('"+_eventTitle+"','"+_eventDesc+"')")
+
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return jsonify(args)
+            else:
+                return {'StatusCode':'400','Bad Request'}
 
         except Exception as e:
             return {'error': str(e)}
-        return {'status': 'success'}
-
 
 class GetEvent(Resource):
     def get(self):
@@ -43,15 +59,19 @@ class GetEvent(Resource):
             return {'error': str(e)}
 
 #Especificação dos endereços/rotas que serão acessados
-@app.route('/CreateEvent')
-def CreateUser(name=None):
+api.add_resource(CreateEvent, '/CreateEvent')
+api.add_resource(GetEvent, '/GetEvent')
+
+@app.route('/CreateEvent', methods=['GET', 'POST'])
+def CreateEvent(name=None):
     return render_template('form1.html', name=name)
 
 @app.route('/GetEvent')
-def GetUser(name=None):
+def GetEvent(name=None):
     return render_template('form2.html', name=name)
 
 if __name__ == '__main__':
     #Biblioteca Waitress como alternativa ao server de desenvolvimento built-in do Flask
     from waitress import serve
     serve(app, host="127.0.0.1", port=8080)
+#    app.run(debug=True)
