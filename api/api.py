d7062dd30016f6858611af5e6e94f679a433d116
fabiodemo <fabiodemo@redes.ufsm.br>
2020-11-15 18:28:29 -0300

    Tratamento dos retornos da busca em json.

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from flaskext.mysql import MySQL
-import datetime
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,7 +26,7 @@ class CreateEvent(Resource):
             #Acessa os identificadores da página html e adiciona uma a uma lista de argumentos
             parser = reqparse.RequestParser()
             parser.add_argument('title', type=str, help='title to create event')
-            parser.add_argument('recipient', type=str, help='description of the event')
+            parser.add_argument('recipient', type=str, help='recipient of the event')
             parser.add_argument('description', type=str, help='description of the event')
             parser.add_argument('date', type=str, help='date of the event')
             parser.add_argument('time', type=str, help='time of the event')
@@ -72,12 +73,14 @@ class GetEvent(Resource):
             parser = reqparse.RequestParser()
             parser.add_argument('id', type=str, help='id to search for the event')
             parser.add_argument('title', type=str, help='title to search for the event')
+            parser.add_argument('recipient', type=str, help='recipient of the event')
             parser.add_argument('date', type=str, help='date of the event')
             parser.add_argument('time', type=str, help='time of the event')
             args = parser.parse_args()
 
             _eventId = args['id']
             _eventTitle = args['title']
+            _eventRec = args['recipient']
             _eventDate = args['date']
             _eventTime = args['time']
 
@@ -85,8 +88,16 @@ class GetEvent(Resource):
             if(request.form['submit'] == 'Buscar'):
                 cursor.execute("SELECT * FROM tblCommunication WHERE Id = %s", (_eventId,))
                 data = cursor.fetchall()
-                return jsonify(data)
-                #return {'StatusCode':'200', 'Message': 'Buscar'}
+                #Tratamento dos resultados da busca, para retornar os dados da forma mais clara possível no json
+                for row in data:
+                    day = row[4].strftime("%d")
+                    #Como no banco de dados a data está em formato yyyy-mm-dd, deve-se realizar a conversão para melhor interpretação
+                    str_date = row[4].strftime("%d-%m-%Y")
+                    str_hour = str(row[5])
+                    means = row[6].lstrip(', ')
+                    return jsonify(id=row[0], title=row[1], recipient=row[2], description = row[3], date=str_date, hour=str_hour, means=means)
+            #return {'StatusCode':'200', 'Message': 'Buscar'}
+
             #Interpretação do botão para deletar determinado agendamento
             elif(request.form['submit'] == 'Deletar'):
                 return {'StatusCode':'200', 'Message': 'Deletar'}
